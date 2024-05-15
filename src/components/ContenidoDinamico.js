8b70ca25620ddbed1d849d59687640a6dcd0c06b
Isaac Avila <isaac.avila.saenz29@gmail.com>
2024-05-15 14:49:12 -0600

    Subiendo el proyecto 2

--- src/components/ContenidoDinamico.js
+++ src/components/ContenidoDinamico.js
@@ -4,22 +4,34 @@ export const ContenidoDinamico = () => {
   const [contenido, setContenido] = useState(
     "AimGYM es un juego de entrenamiento de puntería diseñado para ayudar a los usuarios a mejorar sus habilidades. Ofrece múltiples modalidades que son extremadamente beneficiosas."
   );
+
   let palabras = [
     "AimGYM es un juego de entrenamiento de puntería diseñado para ayudar a los usuarios a mejorar sus habilidades. Ofrece múltiples modalidades que son extremadamente beneficiosas.",
-    "Se planea el lanzamiento de este juego próximamente en el portal de itch.io",
+    <a
+      href="https://murdercrack29.itch.io/aimgym"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Descarga AimGYM aquí
+    </a>,
     "AimGYM es un software creado exclusivamente para entretenimiento y se ofrece de manera gratuita. No se busca lucro con su distribución, ni se generan ingresos a partir de él. Este juego fue desarrollado para proporcionar diversión y entretenimiento a sus usuarios.",
     "Desarrollado por MurderCrack29.",
+    "AimGYM esta disponible ya, aunque esta en un estado sin terminar y sin pulir, dado que es un proyecto sin presupuesto y sin animo de lucro, solo pue por diversion:    ",
   ];
+
   return (
     <div>
       <div className="Botones">
         <button onClick={() => setContenido(palabras[0])}>Juego</button>
-        <button onClick={() => setContenido(palabras[1])}>Descarga</button>
+        <button onClick={() => setContenido([palabras[4], palabras[1]])}>
+          Descarga
+        </button>
+
         <button onClick={() => setContenido(palabras[2])}>Acerca de</button>
         <button onClick={() => setContenido(palabras[3])}>Contacto</button>
       </div>
 
-      <p className="TextoDinamico">{contenido}</p>
+      <div className="TextoDinamico">{contenido}</div>
     </div>
   );
 };
